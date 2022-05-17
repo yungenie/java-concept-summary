596f079c0ec6369dd9960686288a6f1503eb3361
YunJin Gwak <yunjin8931@naver.com>
2022-05-17 17:47:04 +0900

    2022-04-19 [java8] class name example
    하나의 소스파일에 여러 클래스 작성

---
+++ 자바의 정석(인강)/src/Hello.java
@@ -0,0 +1,15 @@
+package lai.module.starter.test;
+
+/**
+ * 하나의 소스파일에 여러 클래스 작성
+ * public class가 하나도 없는 경우, 소스파일의 이름과 클래스명과 상이해도 가능하다.
+ *
+ * @author  Yunjin
+ * @since   JDK1.8
+ */
+class Hello1 {
+    public static void main(String[] args) {
+        System.out.println("Hello1");
+    }
+}
+class Hello2 {}
