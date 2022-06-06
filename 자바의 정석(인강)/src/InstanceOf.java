ccba8d4d6d45e747af2afb00561fbb789d9b45fd
YunJin Gwak <yunjin8931@naver.com>
2022-06-06 21:02:48 +0900

    2022-06-06 [java8] InstanceOf 연산자 예제

--- 자바의 정석(인강)/src/InstanceOf.java
+++ 자바의 정석(인강)/src/InstanceOf.java
@@ -1,5 +1,5 @@
 /**
- * InstanceOf 연산자 예제
+ * Instanceof 연산자 예제
  *
  * @author  Yunjin
  * @since   JDK1.8
@@ -9,21 +9,25 @@ public class InstanceOf {
     public static void main(String[] args) {
         Car car = new Car();
         Car fireEngine = new FireEngine();
+        FireEngine f = new FireEngine();
         Car ambulance = new Ambulance();
-        FireEngine fc = (FireEngine) new Car();
+
+        System.out.println(f instanceof Car); // true
+        System.out.println(car instanceof FireEngine); // false
 
         // InstanceOf 연산자로 형변환 되는 지 확인
-        doWork(car); //error
+        doWork(car); // car instanceof FireEngine/Ambulance => false
         doWork(fireEngine);
         doWork(ambulance);
-        doWork(fc);
+        //doWork(fc);
     }
 
     public static void doWork(Car car) {
         /*
-            인스턴스의 고유한 기능을 사용하기 위해서는 형변환이 필요한데
-            형변환 시, instanceof 연산자를 통해 인스턴스의 타입을 체크할 수 있다.
+            자손객체의 타입을 체크하기 위해 instanceof 연산자를 사용함 (인스턴스의 타입을 체크함)
+            단, 상속관계에서 각 인스턴스의 고유한 기능을 사용하기 위해선 형변환 필요함
          */
+
         if (car instanceof FireEngine) {
             FireEngine fireEngine = (FireEngine) car;
             fireEngine.water();
