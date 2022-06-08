22b7bc8e4901efab322230545a6e23a762e0eaa6
YunJin Gwak <yunjin8931@naver.com>
2022-06-08 22:32:46 +0900

    2022-06-08 [java8] 인터페이스 예제

--- 자바의 정석(인강)/src/Interface.java
+++ 자바의 정석(인강)/src/Interface.java
@@ -9,10 +9,9 @@ public interface Interface {
     /*
         Interface 선언
         Interface란
-        - 추상메서드의 집합
+        - 추상메서드의 집합 (상수, default 메서드, static 메서드)
         - 선언부 메서드 (구현된 것이 없는 설계도, 껍데기)
         - 모든 멤버가 public
-
      */
 
     // 상수 (pulic, static, final 생략가능, 생략해도 붙은 걸로 간주가 됩니다. 컴파일시 붙음)
@@ -24,7 +23,7 @@ public interface Interface {
     // 추상메서드 (public, abstract 생략가능, 항상 인터페이스의 메서드는 public, abstrac)
     public abstract String getName();
     public int getAge();
-    abstract  String getHobby();
+    abstract String getHobby();
 
     //jdk1.8 추가됨 (default 메서드, 본문이 있어야함)
     default String setJOB(int job){
