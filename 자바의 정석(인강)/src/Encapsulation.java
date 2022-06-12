c3c9795e35acae6e54604f36d9800f2fb12b3091
YunJin Gwak <yunjin8931@naver.com>
2022-06-12 18:09:22 +0900

    2022-06-12 [java8] 캡슐화(접근 제어자) 예제

--- 자바의 정석(인강)/src/Encapsulation.java
+++ 자바의 정석(인강)/src/Encapsulation.java
@@ -13,6 +13,9 @@ class Time {
      * - 외부로부터 데이터를 보호하기 위해서
      * - 외부에서는 불필요한, 내부적으로만 사용되는 부분을 감추기 위해서
      *
+     * 접근 제어자
+     * - 클래스는 public > default
+     * - 변수는 public > protected > default > private
      * */
     private int hour;
     private int minute;
