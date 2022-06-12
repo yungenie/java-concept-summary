16306728598ef66074fd04a7f06f8b954b674ac5
YunJin Gwak <yunjin8931@naver.com>
2022-06-12 17:08:57 +0900

    2022-06-12 [java8] 인터페이스 상속 예제
    조상 interface에 동일한 이름을 가지는 default 메서드가 존재할 경우, 충돌 해결 방법

--- 자바의 정석(인강)/src/InterfaceExtends.java
+++ 자바의 정석(인강)/src/InterfaceExtends.java
@@ -22,14 +22,16 @@ class Test implements InterfaceExtends{
 
 public interface InterfaceExtends extends Movable, Attackable{
     /*
-    * Interface 상속
-    * - 인터페이스 상속은 인터페이스만 가능
-    * - 인터페이스의 조상은 인터페이스만 가능 (Object가 최고 조상 아님)
-    * - 다중 상속이 가능 (추상메서드 충돌해도 문제 없음, 클래스는 다중 상속 안됨)
-    * */
+     * Interface 상속
+     * - 인터페이스 상속은 인터페이스만 가능
+     * - 인터페이스의 조상은 인터페이스만 가능 (Object가 최고 조상 아님)
+     * - 다중 상속이 가능 (추상메서드 충돌해도 문제 없음, 클래스는 다중 상속 안됨)
+     * - 두 개의 인터페이스에 동일한 이름을 가지는 default 메서드가 존재할 경우, 충돌 해결 방법
+     * */
 
     default void attack() {
         Movable.getId();
+        // 상속받은 default 메소드들 충돌시 직접 조상 인터페이스 명시
         Movable.super.attack();
     }
 
