bdee3035d8fa5ce142c36af89cdb4965944f9204
YunJin Gwak <yunjin8931@naver.com>
2022-06-12 18:06:23 +0900

    2022-06-12 [java8] 캡슐화(접근 제어자) 예제

---
+++ 자바의 정석(인강)/src/Encapsulation.java
@@ -0,0 +1,44 @@
+/**
+ * 캡슐화 예제
+ *
+ * @author  Yunjin
+ * @since   JDK1.8
+ */
+class Time {
+
+    /**
+     * 캡슐화와 접근 제어자
+     *
+     * 접근 제어자를 사용하는 이유?
+     * - 외부로부터 데이터를 보호하기 위해서
+     * - 외부에서는 불필요한, 내부적으로만 사용되는 부분을 감추기 위해서
+     *
+     * */
+    private int hour;
+    private int minute;
+    private int second;
+
+    public void setHour(int hour){
+        if (isValidHour(hour)) return;
+
+        this.hour = hour;
+
+    }
+    private boolean isValidHour(int hour){
+        return hour < 0 || hour > 23;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Time time = new Time();
+        // error, private 변수 접근 못함.
+        //time.hour = -100;
+        time.setHour(21);
+
+    }
+}
