7f5ecd3af0ce35f7866c0710d9f71e3d0e3eb39a
YunJin Gwak <yungenie8931@gmail.com>
2023-01-11 15:31:49 +0900

    :recycle: Update 2023-01-11 [Java] HashSet<String> add(String s) example code update

--- src/collectionframework/set/hashset/HashSetExam2.java
+++ src/collectionframework/set/hashset/HashSetExam2.java
@@ -6,14 +6,15 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- * An HashSet over a collection.
- * HashSet은 Set 인터페이스의 구현 클래스
- * Set은 한국말로는 "집합"
- * 저장 순서를 유지하지 않음
- * 중복 값을 허용하지 않음
- * 
- * <p> 순서를 유지하고 싶으면 LinkedHashSet 클래스 사용
- * 
+ * HashSet add() 기본 예제
+ *
+ *  <p><blockquote><pre>
+ *  - HashSet : Set인터페이스의 구현 클래스
+ *  - 저장 순서를 유지하지 않고, 중복 값을 허용하지 않음
+ *  - Set은 한국번역어로 "집합"의 의미
+ *  - 순서를 유지하고 싶으면 LinkedHashSet 클래스 사용
+ *  </pre></blockquote></p>
+ *
  * @author yunjin
  * @see Collection
  * @see HashSet
@@ -25,21 +26,25 @@ public class HashSetExam2 {
 	public static void main(String[] args) {
 		
 		// 1. 객체 선언 new HashSet<>();
-		Set<String> set = new HashSet<String>();
-		
+		Set<String> set = new HashSet<>();
+
 		// 2. 데이터 삽입 add()
 		set.add("aspea");
+		set.add("카리나");
+		set.add("카리나");
 		set.add("카리나");
 		set.add("지젤");
 		set.add("윈터");
 		set.add("닝닝");
-		
-		System.out.println(set.toString()); //[윈터, aspea, 닝닝, 지젤, 카리나]
-		System.out.println(set.toString().getClass().getName()); //java.lang.String
-		
-		// 3. 데이터 출력 
+		set.add("닝닝");
+
+		// 3. 데이터 출력
+		// 기본 출력
+		System.out.println(set);
+
+		// Collection 인터페이스의 iterator() 사용
 		Iterator<String> iterSet = set.iterator();
-		
+
 		while(iterSet.hasNext()) {
 			System.out.println(iterSet.next() + "");
 		}
