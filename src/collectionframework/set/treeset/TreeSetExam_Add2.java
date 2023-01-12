ed42f5cd49d071708666d452b2ed91f557a8ffab
YunJin Gwak <yungenie8931@gmail.com>
2023-01-12 21:13:31 +0900

    :seedling: Add 2023-01-12 [Java] TreeSet add() 정렬기준 Comparable 구현 및 compareTo() 오버라이딩

--- src/collectionframework/set/treeset/TreeSetExam_Add2.java
+++ src/collectionframework/set/treeset/TreeSetExam_Add2.java
@@ -10,7 +10,7 @@ import java.util.TreeSet;
  * <p><blockquote><pre>
  *      - Set는 순서를 유지하지 않고 중복을 허용하지 않는 특징이 있습니다.
  *      - 중복 제거를 위해 add() 메서드 내부적으로 비교 후 저장을 합니다.
- *      - 비교할 때 비교 기준이 있어야 하는데, 그 비교 기준을 제공하는 Comparator이 있습니다.
+ *      - 비교할 때 비교 기준이 있어야 하는데, 그 비교 기준을 제공하는 Comparator 인터페이스가 있습니다.
  *      - Comparator 기준이 없으면 저장하는 객체의 Comparable 인터페이스를 구현합니다.
  * </p></blockquote></pre>
  * <ul>
