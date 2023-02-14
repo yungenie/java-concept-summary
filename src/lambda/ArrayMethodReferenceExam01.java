cdb038c5a4e9f31a46317692b30a48331c627b99
YunJin Gwak <yunjin8931@naver.com>
2023-02-14 14:36:19 +0900

    :seedling: Add 2023-02-14 [Java] Lambda - 배열과 메서드 참조 예제 (i의 크기를 가진 배열 생성 후 반환)

---
+++ src/lambda/ArrayMethodReferenceExam01.java
@@ -0,0 +1,31 @@
+package lambda;
+
+import java.util.function.Function;
+
+/**
+ *  람다식 작성하기 - 배열과 메서드 참조 예제 (i의 크기를 가진 배열 생성 후 반환)
+ *
+ *  <p>
+ *  <blockquote><pre>
+ *  - int[](배열)의 연산자(new) 참조.
+ *  - 사용법? {@code 클래스명::연산자}
+ *  - java.util.function 패키지 함수형 인터페이스에서 {@code Function<T,R>} 함수 사용.
+ *      -> 함수형 인터페이스에 정보가 있기 때문에 메서드 T(입력값) 생략 가능.
+ *  - T : Type, R : Return
+ *  </pre></blockquote>
+ *  </p>
+ *  ※ 메서드 참조 -> 람다식으로 변형하는 것도 공부해두기. (입력, 출력 확인)
+ * @author Yunjin
+ * @since 1.8
+ */
+public class ArrayMethodReferenceExam01 {
+    public static void main(String[] args) {
+        Function<Integer, int[]> f2 = (i) -> new int[i];
+        Function<Integer, int[]> ff2 = int[]::new;
+
+        int[] arr = f2.apply(100);
+        System.out.println(arr.length);
+        System.out.println(f2.apply(100).length);
+
+    }
+}
