16ef6512fc8f63e7e9f9c8b1d7a3aa6abce100be
YunJin Gwak <yunjin8931@naver.com>
2023-02-14 14:35:44 +0900

    :seedling: Add 2023-02-14 [Java] Lambda - static 메서드 참조 예제 (문자열을 숫자형으로 변환)

---
+++ src/lambda/MethodReferenceExam01.java
@@ -0,0 +1,28 @@
+package lambda;
+
+import java.util.function.Function;
+
+/**
+ *  람다식 작성하기 - static 메서드 참조 예제 (문자열을 숫자형으로 변환)
+ *  <p>
+ *  <blockquote><pre>
+ *  - Integer(Wrapper Class)의 parseInt(staic 메서드) 참조.
+ *  - 사용법? {@code 클래스명::메서드명}
+ *  - java.util.function 패키지 함수형 인터페이스에서 {@code Function<T,R>} 함수 사용.
+ *      -> 함수형 인터페이스에 정보가 있기 때문에 메서드 T(입력값) 생략 가능.
+ *  - T : Type, R : Return
+ *  </pre></blockquote>
+ *  </p>
+ *  ※ 메서드 참조 -> 람다식으로 변형하는 것도 공부해두기. (입력, 출력 확인)
+ * @author Yunjin
+ * @since 1.8
+ */
+public class MethodReferenceExam01 {
+    public static void main(String[] args) {
+
+        Function<String, Integer> f = (String s) -> Integer.parseInt(s);
+        Function<String, Integer> f1 = Integer::parseInt;
+
+        System.out.println(f1.apply("100")+200);
+    }
+}
