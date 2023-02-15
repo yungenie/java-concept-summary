945c6368a8509320ce285cd6c7910a14d9300bd4
YunJin Gwak <yunjin8931@naver.com>
2023-02-15 17:03:50 +0900

    :seedling: Add 2023-02-15 [Java] 객체 배열 및 기본형 배열로부터 Stream 생성하기

---
+++ src/streamexam/ArrayStreamExam01.java
@@ -0,0 +1,56 @@
+package streamexam;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Stream 만들기 - 객체 배열 및 기본형 배열로부터 Stream 생성하기
+ * <p><blockquote><pre>
+ * 배열 데이터를 스트림으로 받을 때 사용법
+ * 1. 객체 배열의 경우
+ *      - {@code Stream.of()}, {@code Arrays.stream()} 사용
+ *      - return {@code Stream<T>} // T는 타입변수로 참조형
+ * 2. 기본형 배열의 경우
+ *      - {@code 기본형Stream.of()}, {@code Arrays.stream()} 사용
+ *      - return {@code 기본형Stream}
+ * </p></blockquote></pre>
+ *
+ * @since  1.8
+ * @see Arrays
+ * @see IntStream
+ * @see Stream
+ * @author Yunjin
+ */
+public class ArrayStreamExam01 {
+    public static void main(String[] args) {
+
+        // 1. Stream 생성 - 객체 배열로부터 Stream 생성
+        Stream<String> streamStrArgs = Stream.of("1", "2"); // 가변인자 // Stream.of(T) T는 Stream<T> 타입변수
+
+        String[] strArrObj = new String[]{"a", "b", "c", "d"};
+        Stream<String> streamStrArrObj = Stream.of(strArrObj);
+        Stream<String> streamStrArrObj2 = Stream.of(new String[]{"a", "b", "c", "d"});
+        Stream<String> streamStrArrObj3 = Arrays.stream(new String[]{"g", "h", "i"}, 0,2);
+
+        Integer[] integerArrObj = new Integer[]{3, 4, 5};
+        Stream<Integer> streamIntegerArrObj = Stream.of(integerArrObj);
+        Stream<Integer> streamIntegerArrObj2 = Arrays.stream(integerArrObj);
+
+        streamStrArgs.forEach(System.out::print);
+        streamStrArrObj.forEach(System.out::print);
+        streamStrArrObj2.forEach(System.out::print);
+        streamStrArrObj3.forEach(System.out::print);
+        streamIntegerArrObj.forEach(System.out::print);
+        streamIntegerArrObj2.forEach(System.out::print);
+
+        // 2. Stream 생성 - 기본형 배열로부터 Stream 생성
+        int[] intArr = {6, 7, 8, 9, 10, 11};
+        IntStream intStream = IntStream.of(intArr); // IntStream.of(v) v(인자)는 int 타입
+        IntStream intStream1 = Arrays.stream(intArr);
+
+        System.out.print(intStream.count());
+        //System.out.println(intStream.max());
+        //System.out.println(intStream.average());
+    }
+}
