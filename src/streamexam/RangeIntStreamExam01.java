a45ea8067672461805d19a98815c0d1c972763f5
YunJin Gwak <yunjin8931@naver.com>
2023-02-15 17:05:06 +0900

    :seedling: Add 2023-02-15 [Java] 특정 범위의 정수 range(),  rangeClosed() 스트림

---
+++ src/streamexam/RangeIntStreamExam01.java
@@ -0,0 +1,28 @@
+package streamexam;
+
+import java.util.stream.IntStream;
+
+/**
+ * Stream 만들기 - 특정 범위의 정수 range(),  rangeClosed()
+ * <p><blockquote><pre>
+ * 특정 범위의 정수를 요소로 갖는 스트림 생성하기
+ * </p></blockquote></pre>
+ *
+ * @since  1.8
+ * @see IntStream
+ * @author Yunjin
+ */
+public class RangeIntStreamExam01 {
+    public static void main(String[] args) {
+
+        // 특정 범위의 정수 begin ~ end 미만
+        IntStream intStreamRange = IntStream.range(1, 5);
+        intStreamRange.forEach(System.out::print);
+
+        System.out.println("");
+
+        // 특정 범위의 정수 begin ~ end 포함
+        IntStream intStreamRangeClosed = IntStream.rangeClosed(6, 17);
+        intStreamRangeClosed.forEach(System.out::print);
+    }
+}
