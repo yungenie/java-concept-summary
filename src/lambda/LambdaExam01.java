1ff4c9809c6014909b57c75d5be9981f9be7d3d9
YunJin Gwak <yunjin8931@naver.com>
2023-02-15 17:14:13 +0900

    :memo: Update 2023-02-15 [Java] Lambda - 함수형 인터페이스를 활용한 익명객체 및 람다식 비교

--- src/lambda/LambdaExam01.java
+++ src/lambda/LambdaExam01.java
@@ -1,7 +1,7 @@
 package lambda;
 
 /**
- *  람다식 작성하기 - 익명객체와 함수형 인터페이스를 사용한 람다식 비교(익명객체 vs 함수형 인터페이스)
+ *  Lambda - 함수형 인터페이스를 활용한 익명객체 및 람다식 비교
  *  <p>
  *  <blockquote><pre>
  *  - 람다식(익명 객체)을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
@@ -9,6 +9,7 @@ package lambda;
  *  </p>
  *
  * @author Yunjin
+ * @see FunctionalInterface
  * @since 1.8
  */
 public class LambdaExam01 {
@@ -21,21 +22,19 @@ public class LambdaExam01 {
             }
         };
 
-        //int value = obj.max(3, 5); //error
+        //int value = obj.max(3, 5); //error. Object 클래스에는 max 함수가 없음.
 
-        // 익명 객체 만들기
+        // 함수형 인터페이스 - 익명 객체 만들기
         MyFunction myFunc =  new MyFunction() {
             @Override
             public int max(int a, int b) { // 오버라이딩 - 접근 제어자는 좁게 못바꾼다.
                 return 0;
             }
         };
-        int myFuncResult = myFunc.max(5, 17);
+        int myFuncResult1 = myFunc.max(5, 17);
 
-        MyFunction myFunc2 = (a, b) -> a > b ? a : b;
-        int myFuncResult2 = myFunc2.max(5, 17);
 
-        // 익명 객체 람다식 사용하기
+        // 함수형 인터페이스 - 람다식 사용하기
         MyFunction myFuncLamdba =  (a,b) -> a > b? a : b;
         int myFuncLamdbaResult = myFuncLamdba.max(3, 5);
 
