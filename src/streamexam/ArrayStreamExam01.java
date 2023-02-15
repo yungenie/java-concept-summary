0014e7ebd3add7f95b03aca2204b7e16a7f21514
YunJin Gwak <yunjin8931@naver.com>
2023-02-15 17:11:11 +0900

    :memo: Update 2023-02-15 [Java] Stream 만들기 - 객체 배열 및 기본형 배열로부터 스트림 생성하기

--- src/streamexam/ArrayStreamExam01.java
+++ src/streamexam/ArrayStreamExam01.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * Stream 만들기 - 객체 배열 및 기본형 배열로부터 Stream 생성하기
+ * Stream 만들기 - 객체 배열 및 기본형 배열로부터 스트림 생성하기
  * <p><blockquote><pre>
  * 배열 데이터를 스트림으로 받을 때 사용법
  * 1. 객체 배열의 경우
@@ -26,7 +26,8 @@ public class ArrayStreamExam01 {
     public static void main(String[] args) {
 
         // 1. Stream 생성 - 객체 배열로부터 Stream 생성
-        Stream<String> streamStrArgs = Stream.of("1", "2"); // 가변인자 // Stream.of(T) T는 Stream<T> 타입변수
+        // Stream.of(T) T는 Stream<T> 타입변수
+        Stream<String> streamStrArgs = Stream.of("1", "2"); // 가변인자
 
         String[] strArrObj = new String[]{"a", "b", "c", "d"};
         Stream<String> streamStrArrObj = Stream.of(strArrObj);
@@ -45,8 +46,9 @@ public class ArrayStreamExam01 {
         streamIntegerArrObj2.forEach(System.out::print);
 
         // 2. Stream 생성 - 기본형 배열로부터 Stream 생성
+        // IntStream.of(v) v(인자)는 int 타입
         int[] intArr = {6, 7, 8, 9, 10, 11};
-        IntStream intStream = IntStream.of(intArr); // IntStream.of(v) v(인자)는 int 타입
+        IntStream intStream = IntStream.of(intArr);
         IntStream intStream1 = Arrays.stream(intArr);
 
         System.out.print(intStream.count());
