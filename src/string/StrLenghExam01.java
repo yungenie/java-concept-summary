a66b291b5eb2e3d5bb8c75238e2fedd2b261cf57
YunJin Gwak <yunjin8931@naver.com>
2023-02-25 18:00:21 +0900

    :seedling: Add 2023-02-25 [Java] Stream - Collectors 그룹핑 및 통계 예제 (groupingBy)

---
+++ src/string/StrLenghExam01.java
@@ -0,0 +1,11 @@
+package string;
+
+public class StrLenghExam01 {
+    public static void main(String[] args) {
+        String str = " ";
+        System.out.println("str.length() = " + str.length()); //문자열의 문자길이 반환
+
+        String s = "123.2";
+        System.out.println(s.indexOf("3.2"));
+    }
+}
